1f2e253aa11d47c2b416add25a2edd5703380e24
JosephTesta <jtesta94@yahoo.com>
2016-04-21 22:10:54 -0400

    Update State_Count_ranking.py

--- President_Project/State_Count_ranking.py
+++ President_Project/State_Count_ranking.py
@@ -13,109 +13,208 @@ def state_count(name):
     textAI = SA.analyzer()
     while line:
         a = line.lower()
-        if (textAI.getSingleSentiment(line) == 'pos'):
-
             if " al\"" in a or "alabama" in a:
-                states['AL'] += 1
+                line = searchfile.readline()
+                if (textAI.getSingleSentiment(line) == 'pos'):
+                    states['AL'] += 1
             if " ak\"" in a or "alaska" in a:
-                states['AK'] += 1
+                line = searchfile.readline()
+                if (textAI.getSingleSentiment(line) == 'pos'):
+                    states['AK'] += 1
             if " az\"" in a or "arizona" in a:
-                states['AZ'] += 1
+                line = searchfile.readline()
+                if (textAI.getSingleSentiment(line) == 'pos'):
+                    states['AZ'] += 1
             if " ar\"" in a or "arkansas" in a:
-                states['AR'] += 1
+                line = searchfile.readline()
+                if (textAI.getSingleSentiment(line) == 'pos'):
+                    states['AR'] += 1
             if " ca\"" in a or "california" in a:
-                states['CA'] += 1
+                line = searchfile.readline()
+                if (textAI.getSingleSentiment(line) == 'pos'):
+                    states['CA'] += 1
             if " co\"" in a or "colorado" in a:
-                states['CO'] += 1
+                line = searchfile.readline()
+                if (textAI.getSingleSentiment(line) == 'pos'):
+                    states['CO'] += 1
             if " ct\"" in a or "connecticut" in a:
-                states['CT'] += 1
+                line = searchfile.readline()
+                if (textAI.getSingleSentiment(line) == 'pos'):
+                    states['CT'] += 1
             if " de\"" in a or "delaware" in a:
-                states['DE'] += 1
+                line = searchfile.readline()
+                if (textAI.getSingleSentiment(line) == 'pos'):
+                    states['DE'] += 1
             if " fl\"" in a or "florida" in a:
-                states['FL'] += 1
+                line = searchfile.readline()
+                if (textAI.getSingleSentiment(line) == 'pos'):
+                    states['FL'] += 1
             if " ga\"" in a or "georgia" in a:
-                states['GA'] += 1
+                line = searchfile.readline()
+                if (textAI.getSingleSentiment(line) == 'pos'):
+                    states['GA'] += 1
             if " hi\"" in a or "hawaii" in a:
-                states['HI'] += 1
+                line = searchfile.readline()
+                if (textAI.getSingleSentiment(line) == 'pos'):
+                    states['HI'] += 1
             if " id\"" in a or "idaho" in a:
-                states['ID'] += 1
+                line = searchfile.readline()
+                if (textAI.getSingleSentiment(line) == 'pos'):
+                    states['ID'] += 1
             if " il\"" in a or "illinois" in a:
-                states['IL'] += 1
+                line = searchfile.readline()
+                if (textAI.getSingleSentiment(line) == 'pos'):
+                    states['IL'] += 1
             if " in\"" in a or "indiana" in a:
-                states['IN'] += 1
+                line = searchfile.readline()
+                if (textAI.getSingleSentiment(line) == 'pos'):
+                    states['IN'] += 1
             if " ia\"" in a or "iowa" in a:
-                states['IA'] += 1
+                line = searchfile.readline()
+                if (textAI.getSingleSentiment(line) == 'pos'):
+                    states['IA'] += 1
             if " ks\"" in a or "kansas" in a:
-                states['KS'] += 1
+                line = searchfile.readline()
+                if (textAI.getSingleSentiment(line) == 'pos'):
+                    states['KS'] += 1
             if " ky\"" in a or "kentucky" in a:
-                states['KY'] += 1
+                line = searchfile.readline()
+                if (textAI.getSingleSentiment(line) == 'pos'):
+                    states['KY'] += 1
             if " la\"" in a or "louisiana" in a:
-                states['LA'] += 1
+                line = searchfile.readline()
+                if (textAI.getSingleSentiment(line) == 'pos'):
+                    states['LA'] += 1
             if " me\"" in a or "maine" in a:
-                states['ME'] += 1
+                line = searchfile.readline()
+                if (textAI.getSingleSentiment(line) == 'pos'):
+                    states['ME'] += 1
             if " md\"" in a or "maryland" in a:
-                states['MD'] += 1
+                line = searchfile.readline()
+                if (textAI.getSingleSentiment(line) == 'pos'):
+                    states['MD'] += 1
             if " ma\"" in a or "massachusetts" in a:
-                states['MA'] += 1
+                line = searchfile.readline()
+                if (textAI.getSingleSentiment(line) == 'pos'):
+                    states['MA'] += 1
             if " mi\"" in a or "michigan" in a:
-                states['MI'] += 1
+                line = searchfile.readline()
+                if (textAI.getSingleSentiment(line) == 'pos'):
+                    states['MI'] += 1
             if " mn\"" in a or "minnesota" in a:
-                states['MN'] += 1
+                line = searchfile.readline()
+                if (textAI.getSingleSentiment(line) == 'pos'):
+                    states['MN'] += 1
             if " ms\"" in a or "mississippi" in a:
-                states['MS'] += 1
+                line = searchfile.readline()
+                if (textAI.getSingleSentiment(line) == 'pos'):
+                    states['MS'] += 1
             if " mo\"" in a or "missouri" in a:
-                states['MO'] += 1
+                line = searchfile.readline()
+                if (textAI.getSingleSentiment(line) == 'pos'):
+                    states['MO'] += 1
             if " mt\"" in a or "montana" in a:
-                states['MT'] += 1
+                line = searchfile.readline()
+                if (textAI.getSingleSentiment(line) == 'pos'):
+                    states['MT'] += 1
             if " ne\"" in a or "nebraska" in a:
-                states['NE'] += 1
+                line = searchfile.readline()
+                if (textAI.getSingleSentiment(line) == 'pos'):
+                    states['NE'] += 1
             if " nv\"" in a or "nevada" in a:
-                states['NV'] += 1
+                line = searchfile.readline()
+                if (textAI.getSingleSentiment(line) == 'pos'):
+                    states['NV'] += 1
             if " nh\"" in a or "new hampshire" in a:
-                states['NH'] += 1
+                line = searchfile.readline()
+                if (textAI.getSingleSentiment(line) == 'pos'):
+                    states['NH'] += 1
             if " nj\"" in a or "new jersey" in a:
-                states['NJ'] += 1
+                line = searchfile.readline()
+                if (textAI.getSingleSentiment(line) == 'pos'):
+                    states['NJ'] += 1
             if " nm\"" in a or "new mexico" in a:
-                states['NM'] += 1
+                line = searchfile.readline()
+                if (textAI.getSingleSentiment(line) == 'pos'):
+                    states['NM'] += 1
             if " ny\"" in a or "new york" in a:
-                states['NY'] += 1
+                line = searchfile.readline()
+                if (textAI.getSingleSentiment(line) == 'pos'):
+                    states['NY'] += 1
             if " nc\"" in a or "north carolina" in a:
-                states['NC'] += 1
+                line = searchfile.readline()
+                if (textAI.getSingleSentiment(line) == 'pos'):
+                    states['NC'] += 1
             if " nd\"" in a or "north dakota" in a:
-                states['ND'] += 1
+                line = searchfile.readline()
+                if (textAI.getSingleSentiment(line) == 'pos'):
+                    states['ND'] += 1
             if " oh\"" in a or "ohio" in a:
-                states['OH'] += 1
+                line = searchfile.readline()
+                if (textAI.getSingleSentiment(line) == 'pos'):
+                    states['OH'] += 1
             if " ok\"" in a or "oklahoma" in a:
-                states['OK'] += 1
+                line = searchfile.readline()
+                if (textAI.getSingleSentiment(line) == 'pos'):
+                    states['OK'] += 1
             if " or\"" in a or "oregon" in a:
-                states['OR'] += 1
+                line = searchfile.readline()
+                if (textAI.getSingleSentiment(line) == 'pos'):
+                    states['OR'] += 1
             if " pa\"" in a or "pennsylvania" in a:
-                states['PA'] += 1
+                line = searchfile.readline()
+                if (textAI.getSingleSentiment(line) == 'pos'):
+                    states['PA'] += 1
             if " ri\"" in a or "rhode island" in a:
-                states['RI'] += 1
+                line = searchfile.readline()
+                if (textAI.getSingleSentiment(line) == 'pos'):
+                    states['RI'] += 1
             if " sc\"" in a or "south carolina" in a:
-                states['SC'] += 1
+                line = searchfile.readline()
+                if (textAI.getSingleSentiment(line) == 'pos'):
+                    states['SC'] += 1
             if " sd\"" in a or "south dakota" in a:
-                states['SD'] += 1
+                line = searchfile.readline()
+                if (textAI.getSingleSentiment(line) == 'pos'):
+                    states['SD'] += 1
             if " tn\"" in a or "tennessee" in a:
-                states['TN'] += 1
+                line = searchfile.readline()
+                if (textAI.getSingleSentiment(line) == 'pos'):
+                    states['TN'] += 1
             if " tx\"" in a or "texas" in a:
-                states['TX'] += 1
+                line = searchfile.readline()
+                if (textAI.getSingleSentiment(line) == 'pos'):
+                    states['TX'] += 1
             if " ut\"" in a or "utah" in a:
-                states['UT'] += 1
+                line = searchfile.readline()
+                if (textAI.getSingleSentiment(line) == 'pos'):
+                    states['UT'] += 1
             if " vt\"" in a or "vermont" in a:
-                states['VT'] += 1
+                line = searchfile.readline()
+                if (textAI.getSingleSentiment(line) == 'pos'):
+                    states['VT'] += 1
             if " va\"" in a or "virginia" in a:
-                states['VA'] += 1
+                line = searchfile.readline()
+                if (textAI.getSingleSentiment(line) == 'pos'):
+                    states['VA'] += 1
             if " wa\"" in a or "washington" in a:
-                states['WA'] += 1
+                line = searchfile.readline()
+                if (textAI.getSingleSentiment(line) == 'pos'):
+                    states['WA'] += 1
             if " wv\"" in a or "west virginia" in a:
-                states['WV'] += 1
+                line = searchfile.readline()
+                if (textAI.getSingleSentiment(line) == 'pos'):
+                    states['WV'] += 1
             if " wi\"" in a or "wisconsin" in a:
-                states['WI'] += 1
+                line = searchfile.readline()
+                if (textAI.getSingleSentiment(line) == 'pos'):
+                    states['WI'] += 1
             if " wy\"" in a or "wyoming" in a:
-                states['WY'] += 1
-        line = searchfile.readline() #//
+                line = searchfile.readline()
+                if (textAI.getSingleSentiment(line) == 'pos'):
+                    states['WY'] += 1
+            else:
+                line = searchfile.readline()
         line = searchfile.readline()
     searchfile.close()
     return states
